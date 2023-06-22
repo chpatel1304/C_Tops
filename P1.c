a3f71c5992eb1e9adc50fb381c4358159531ac31
C_Tops <chpatel1304@gmail.com>
2023-06-22 16:30:38 +0530

    Done

---
+++ P1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+char a[20];
+printf("Enter Your Name : ");
+scanf("%s",&a);
+int b;
+printf("Enter Your Age : ");
+scanf("%d",&b);
+printf("Your Name is %s \n",a);	
+printf("Your Age is %d ",b);
+}
