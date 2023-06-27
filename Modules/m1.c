33c5405b391778f5fa89dd914cd250b0772f4c37
C_Tops <chpatel1304@gmail.com>
2023-06-27 18:41:03 +0530

    Done

--- Modules/m1.c
+++ Modules/m1.c
@@ -11,12 +11,16 @@ main(){
   char add[50];
   printf("Enter Your Name :");
   scanf("%s",&name);
+  fflush(stdin);
   printf("Enter Your DOB :");
   scanf("%l",&DOB);
+  fflush(stdin);
   printf("Enter Your Age :");
   scanf("%d",&age);
+  fflush(stdin);
   printf("Enter Your Address :");
   scanf("%s",&add);
+  fflush(stdin);
   printf("\nYour Name is %s ",name);
  printf("\nYour DOB is %l ",DOB); 	
   printf("\n Your Age is %d ",age);
