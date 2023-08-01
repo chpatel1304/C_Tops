ecba2d4ae712546c3fafb85ab0fa579787967a3b
C_Tops <chpatel1304@gmail.com>
2023-08-01 19:38:55 +0530

    Done

---
+++ Lectures/C++/P1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num,num1;
+	cout<<"Enter Number 1 :";
+	cin>>num;
+	cout<<"Enter Number 2 :";
+	cin>>num1;
+	cout<<num<<" + "<<num1<<" = "<<num+num1;
+	return 0;
+}
---
+++ Lectures/C++/P2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class person{
+	public:
+		void display(){
+			cout<<"This is Person Class";
+		}
+};
+int main (){
+    person obj;
+    obj.display();
+	return 0;
+}
